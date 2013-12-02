cdcdc0d90af1e3d3cdb07a900558d95b17bc0128
Borja Sotomayor <borja@cs.uchicago.edu>
2013-12-02 16:16:12 -0600

    Added an exception class

--- src/chisubmit/__init__.py
+++ src/chisubmit/__init__.py
@@ -6,3 +6,6 @@ RELEASE="0.1.0"
 DEFAULT_CHISUBMIT_DIR = os.path.expanduser("~/.chisubmit/")
 DEFAULT_CONFIG_FILE = DEFAULT_CHISUBMIT_DIR + "chisubmit.conf"
 DEFAULT_COURSE_FILENAME = "default_course"
+
+class ChisubmitException(Exception):
+    pass

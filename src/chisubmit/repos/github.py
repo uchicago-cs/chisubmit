6d9d36037d8160f03e94a90ec4890738b2785347
Borja Sotomayor <borja@cs.uchicago.edu>
2014-07-21 18:28:08 -0500

    GitHub driver connects correctly

--- src/chisubmit/repos/github.py
+++ src/chisubmit/repos/github.py
@@ -1,9 +1,21 @@
+
+# Needed so we can import from the global "github" package
+from __future__ import absolute_import
+
+from github import Github
+from github.GithubException import GithubException
+
+import chisubmit.core
 from chisubmit.repos import RemoteRepositoryConnectionBase
+from chisubmit.core import ChisubmitException
 
 class GitHubConnection(RemoteRepositoryConnectionBase):
 
     def __init__(self, connection_string):
-        RemoteRepositoryConnectionBase.__init__(self, connection_string)        
+        RemoteRepositoryConnectionBase.__init__(self, connection_string)   
+        
+        self.organization = None   
+        self.gh = None  
 
     @staticmethod
     def get_server_type_name():
@@ -11,9 +23,27 @@ class GitHubConnection(RemoteRepositoryConnectionBase):
     
     @staticmethod
     def get_connstr_mandatory_params():
-        return ["organization"]
+        return ["github_organization"]
 
     @staticmethod
     def get_connstr_optional_params():
         return []
 
+    def connect(self):
+        github_access_token = chisubmit.core.get_github_token()
+
+        if github_access_token is None:
+            raise ChisubmitException("Not GitHub access token found.")
+        
+        self.gh = Github(github_access_token)
+        
+        try:
+            self.organization = self.gh.get_organization(self.github_organization)
+        except GithubException as ge:
+            if ge.status == 401:
+                raise ChisubmitException("Invalid Github Credentials", ge)
+            elif ge.status == 404:
+                raise ChisubmitException("Organization %s does not exist" % self.github_organization, ge)            
+            else:
+                raise ChisubmitException("Unexpected error accessing organization %s (%i: %s)" % (self.github_organization, ge.status, ge.data["message"]), ge)            
+        

bc0e55db1dbaa4ac19ebabe6eeace9d88765fcff
chudler <chudler@cs.uchicago.edu>
2014-12-17 13:17:41 -0600

    Provide an actual person update validation form

--- src/chisubmit/backend/webapp/api/people/forms.py
+++ src/chisubmit/backend/webapp/api/people/forms.py
@@ -7,9 +7,24 @@ from wtforms.fields import StringField
 class CreatePersonInput(Form):
     id = StringField(validators=[Length(max=36), InputRequired()])
     first_name = StringField(validators=[Length(max=36), InputRequired()])
-    last_name = StringField('course_id',
+    last_name = StringField('last_name',
                             validators=[Length(max=36, min=5),
                                         InputRequired()])
+    email = StringField(
+        validators=[InputRequired(), Length(max=128, min=10), Email()])
+    git_server_id = StringField('git_server_id',
+                                validators=[Length(max=36, min=5),
+                                            Optional()])
+    git_staging_server_id = StringField('git_staging_server_id',
+                                        validators=[Length(max=36, min=5),
+                                                    Optional()])
+
+
+class UpdatePersonInput(Form):
+    first_name = StringField(validators=[Length(max=36), InputRequired()])
+    last_name = StringField('last_name',
+                            validators=[Length(max=36, min=5),
+                                        Optional()])
     email = StringField(
         validators=[InputRequired(), Length(max=128, min=10), Email()])
     git_server_id = StringField('git_server_id',

807b6466180dbf41cd41204067a0f708185b1708
chudler <chudler@cs.uchicago.edu>
2014-12-17 13:09:33 -0600

    Simplest possible LDAP integration just to get started

---
+++ src/chisubmit/backend/webapp/auth/ldapclient.py
@@ -0,0 +1,21 @@
+import ldap
+from ldap.filter import filter_format
+
+global conn
+ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+conn = ldap.initialize('ldaps://ldap.cs.uchicago.edu')
+conn.protocol_version = ldap.VERSION3
+
+
+def authenticate(username, password):
+    dn = user_dn(username)
+    conn.simple_bind(dn, password)
+
+
+def user_dn(uid):
+    print "USER IS %s" % uid
+    search_filter = filter_format("uid=%s", (uid,))
+    return next(user[0]
+                for user in conn.search_s('dc=uchicago,dc=edu',
+                                          ldap.SCOPE_SUBTREE,
+                                          search_filter, []))

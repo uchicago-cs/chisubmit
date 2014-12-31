60ebacd5e4d605418ee3b0f8e269727395235f50
Borja Sotomayor <borja@cs.uchicago.edu>
2014-12-30 18:25:14 -0600

    Fixed unicode warning

--- src/chisubmit/backend/webapp/api/types.py
+++ src/chisubmit/backend/webapp/api/types.py
@@ -11,7 +11,7 @@ class JSONEncodedDict(TypeDecorator):
 
     def process_bind_param(self, value, dialect):
         if value is not None:
-            value = json.dumps(value)
+            value = unicode(json.dumps(value))
         return value
 
     def process_result_value(self, value, dialect):

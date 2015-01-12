c063ca307f4d9b212ea2aeb2afaab36386577d58
Borja Sotomayor <borja@cs.uchicago.edu>
2015-01-12 17:40:32 -0600

    'instructor team pull-repos' can now pull just the submitted repos

--- src/chisubmit/cli/instructor/team.py
+++ src/chisubmit/cli/instructor/team.py
@@ -9,6 +9,7 @@ import os
 from chisubmit.repos.local import LocalGitRepo
 from chisubmit.common.utils import create_connection
 from git.exc import InvalidGitRepositoryError, GitCommandError
+import operator
 
 @click.group(name="team")
 @click.pass_context
@@ -42,11 +43,12 @@ def instructor_team_search(ctx, course, verbose, team_id):
 @click.command(name="pull-repos")
 @click.argument('assignment_id', type=str)
 @click.argument('directory', type=str)
+@click.option('--only-submitted', is_flag=True)
 @click.option('--reset', is_flag=True)
 @click.option('--only', type=str)
 @pass_course
 @click.pass_context
-def instructor_team_pull_repos(ctx, course, assignment_id, directory, reset, only):
+def instructor_team_pull_repos(ctx, course, assignment_id, directory, only_submitted, reset, only):
     assignment = course.get_assignment(assignment_id)
     if assignment is None:
         print "Assignment %s does not exist" % assignment_id
@@ -65,36 +67,51 @@ def instructor_team_pull_repos(ctx, course, assignment_id, directory, reset, onl
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    for team in [t for t in teams if t.active]:
+    max_len = max([len(t.id) for t in teams])
+
+    for team in sorted([t for t in teams if t.active], key=operator.attrgetter("id")):
         team_dir = "%s/%s" % (directory, team.id)
-        team_git_url = conn.get_repository_git_url(course, team)
-        if not os.path.exists(team_dir):
-            print "Cloning repo for %s" % team.id
-            LocalGitRepo.create_repo(team_dir, clone_from_url=team_git_url)
-        else:
-            try:
+        team_git_url = conn.get_repository_git_url(course, team) 
+        ta = team.get_assignment(assignment.id)
+
+        if ta.submitted_at is None and only_submitted:
+            print "%-*s  SKIPPING (not submitted)" % (max_len, team.id)
+            continue
+        
+        try:
+            msg = ""
+            if not os.path.exists(team_dir):
+                r = LocalGitRepo.create_repo(team_dir, clone_from_url=team_git_url)
+                msg = "Cloned repo"
+            else:
                 r = LocalGitRepo(team_dir)
                 if reset:
                     r.fetch("origin")
                     r.reset_branch("origin", "master")
-                    print "Reset %s to match origin/master" % team.id
+                    msg = "Reset to match origin/master" 
                 else:
                     if r.repo.is_dirty():
-                        print "ERROR: Cannot pull %s. It has unstaged changes." % team.id
+                        print "%-*s  ERROR: Cannot pull. Local repository has unstaged changes." % (max_len, team.id)
                         continue
                     r.checkout_branch("master")
                     r.pull("origin", "master")
-                    print "Pulled latest changes for %s" % team.id
-            except ChisubmitException, ce:
-                print "ERROR: Could not checkout or pull master branch for %s (%s)" % (team.id, ce.message)
-            except GitCommandError, gce:
-                print "ERROR: Could not checkout or pull master branch for %s" % (team.id)
-                print gce
-            except InvalidGitRepositoryError, igre:
-                print "ERROR: Directory %s exists but does not contain a valid git repository"
-            except Exception, e:
-                print "ERROR: Unexpected exception when trying to checkout/pull %s" % team.id
+                    msg = "Pulled latest changes"
+            if only_submitted:
+                r.checkout_commit(ta.commit_sha)
+                msg += " and checked out commit %s" % (ta.commit_sha)               
+            print "%-*s  %s" % (max_len, team.id, msg)
+        except ChisubmitException, ce:
+            print "%-*s  ERROR: Could not checkout or pull master branch (%s)" % (max_len, team.id, ce.message)
+        except GitCommandError, gce:
+            print "%-*s  ERROR: Could not checkout or pull master branch" % (max_len, team.id)
+            print gce
+        except InvalidGitRepositoryError, igre:
+            print "%-*s  ERROR: Directory %s exists but does not contain a valid git repository"  % (max_len, team.id, team_dir)
+        except Exception, e:
+            print "%-*s  ERROR: Unexpected exception when trying to checkout/pull" % (max_len, team.id)
+            raise
     
+        
 
     return CHISUBMIT_SUCCESS
 

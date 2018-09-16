2b20ff42e578cf90ad3d9587f10b4aea1efe7fc6
Borja Sotomayor <borja@cs.uchicago.edu>
2018-09-16 18:17:39 -0500

    Updated setup.py + version bump

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@
 #
 # -------------------------------------------------------------------------- #
 
-from ez_setup import use_setuptools
-use_setuptools(version="3.1")
 from setuptools import setup, find_packages
 import sys
 
